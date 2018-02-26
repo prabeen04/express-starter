a1a091bde2e6064c5ad2db67b0e232f0c6f8dbec
Prabeen04 <prabeen.strange@gmail.com>
2018-02-26 23:17:35 +0530

    posts schema added

---
+++ models/posts.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+var schema = mongoose.schema;
+
+var PostSchema = new schema({
+    title: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    body: {
+        type: String,
+    }
+})
+
+var PostModel = mongoose.model('posts', PostSchema);
+module.exports = PostModel;

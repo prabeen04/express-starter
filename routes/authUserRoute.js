c9263c7019a27c17484b281c974e71211d615082
Prabeen04 <prabeen.strange@gmail.com>
2018-02-27 21:40:56 +0530

    login authentication with password match added

--- routes/authUserRoute.js
+++ routes/authUserRoute.js
@@ -7,6 +7,8 @@ const { matchedData, sanitize } = require('express-validator/filter');
 var bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+
+// GET ALL THE USERS FROM THE AUTHENTICATED USERS
 router.get('/register', function (req, res, next) {
     AuthUser.find()
         .then(users => {
@@ -14,6 +16,7 @@ router.get('/register', function (req, res, next) {
         }).catch(next);
 })
 
+// REGISTRATION SYSTEM IN EXPRESS
 router.post('/register',
     [
         check('username')
@@ -53,6 +56,48 @@ router.post('/register',
             });
         });
 
+    })
+
+// LOGIN SYSTEM IN EXPRESS
+router.post('/login',
+    [
+        check('email')
+            .exists().withMessage('email is required')
+        // .custom(value => {
+        //     return findUserByEmail(value).then(user => {
+        //         throw new Error('this email is already in use');
+        //     })
+        // })
+
+        // General error messages can be given as a 2nd argument in the check APIs
+        // check('password', 'passwords must be at least 8 chars long and contain one number')
+        //     .isLength({ min: 8 })
+        //     .matches(/\d/),
+    ],
+    function (req, res, next) {
+        // check for validation error server-side and throw error
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.mapped() });
+        }
+
+        var authuser = {
+            email: req.body.email,
+        }
+
+        AuthUser.findOne(authuser)
+            .then(user => {
+                bcrypt.compare(req.body.password, user.password, function (err, response) {
+                    if(err){
+                        console.log(err);
+                    }
+                    // console.log(response);
+                    if(response){
+                        res.status(200).send(user.email);
+                    }
+                })
+            })
+            .catch(next)
     })
 
 module.exports = router;

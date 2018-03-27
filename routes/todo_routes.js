5a4ace936aa40e010eb46e39e3f5a220001dc2ff
Prabeen04 <prabeen.strange@gmail.com>
2018-03-27 08:38:11 +0530

    todo routes created

---
+++ routes/todo_routes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+var Todos = require('../models/todos');
+
+
+//GET request to /todos
+router.get('/todos', function (req, res) {
+  Todos.find()
+        .then(todos => {
+            res.status(200).send(todos)
+            next()
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
+});
+
+//todo request to /todos
+router.todo('/todos', function (req, res, next) {
+    var todos = (req.body);
+  Todos.create(todos)
+        .then(todo => {
+            res.status(200).send(todo)
+        })
+        .catch(next)
+
+});
+
+//PUT request to /todos/:id
+router.put('/todos/:id', function (req, res, next) {
+  Todos.findByIdAndUpdate({ "_id": req.params.id }, req.body)
+        .then(() => {
+          Todos.findOne({ "_id": req.params.id })
+                .then(todo => {
+                    res.status(200).send(todo);
+                })
+                .catch(next)
+        })
+        .catch(next)
+});
+
+//DELETE request to /todos/:id
+router.delete('/todos/:id', function (req, res, next) {
+  Todos.findByIdAndRemove({ "_id": req.params.id })
+        .then((todo) => {
+            res.status(200).send(todo);
+        })
+        .catch(next)
+});
+
+module.exports = router;

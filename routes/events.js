429be1b0d268860613f6388bcc085085e82d6481
Prabeen04 <prabeen.strange@gmail.com>
2018-04-03 08:32:01 +0530

    event route added

---
+++ routes/events.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+var Events = require('../models/events');
+
+//GET request to /
+router.get('/', function (req, res) {
+    res.send(`<h1>Congratulations......</h1>`)
+});
+
+//GET request to /events
+router.get('/events', function (req, res) {
+    Events.find()
+        .then(events => {
+            res.status(200).send(events)
+            next()
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        })
+});
+
+//event request to /events
+router.event('/events', function (req, res, next) {
+    var events = (req.body);
+    Events.create(events)
+        .then(event => {
+            res.status(200).send(event)
+        })
+        .catch(next)
+
+});
+
+//PUT request to /events/:id
+router.put('/events/:id', function (req, res, next) {
+    Events.findByIdAndUpdate({ "_id": req.params.id }, req.body)
+        .then(() => {
+            Events.findOne({ "_id": req.params.id })
+                .then(event => {
+                    res.status(200).send(event);
+                })
+                .catch(next)
+        })
+        .catch(next)
+});
+
+//DELETE request to /events/:id
+router.delete('/events/:id', function (req, res, next) {
+    Events.findByIdAndRemove({ "_id": req.params.id })
+        .then((event) => {
+            res.status(200).send(event);
+        })
+        .catch(next)
+});
+
+module.exports = router;

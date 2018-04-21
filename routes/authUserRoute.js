a0d7a6075f67a41c462baba3e5536bc785be0673
Prabeen04 <prabeen.strange@gmail.com>
2018-04-21 11:42:40 +0530

    jwt error fixes

--- routes/authUserRoute.js
+++ routes/authUserRoute.js
@@ -14,24 +14,24 @@ var JwtStrategy = passportJWT.Strategy;
 var jwt = require('jsonwebtoken');
 
 var opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'chicharito14';
 // opts.issuer = 'accounts.examplesoft.com';
 // opts.audience = 'yoursite.net';
-console.log(ExtractJwt)
-passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findOne({ id: jwt_payload.sub }, function (err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
-    });
-}));
+console.log(opts)
+// passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+//     User.findOne({ id: jwt_payload.sub }, function (err, user) {
+//         if (err) {
+//             return done(err, false);
+//         }
+//         if (user) {
+//             return done(null, user);
+//         } else {
+//             return done(null, false);
+//             // or you could create a new account
+//         }
+//     });
+// }));
 passport.use(new LocalStrategy(
     { usernameField: "email", passwordField: "password" },
     function (username, password, done) {
